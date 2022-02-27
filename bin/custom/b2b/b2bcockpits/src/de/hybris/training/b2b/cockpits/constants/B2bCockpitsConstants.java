3e259e7e494d00e291da61fe813c45fa03959d0c
pavananumoju <pavananumoju@gmail.com>
2022-02-27 22:39:46 +0530

    Initial B2B Code commit OOTB

---
+++ bin/custom/b2b/b2bcockpits/src/de/hybris/training/b2b/cockpits/constants/B2bCockpitsConstants.java
@@ -0,0 +1,26 @@
+/*
+ * [y] hybris Platform
+ *
+ * Copyright (c) 2017 SAP SE or an SAP affiliate company.  All rights reserved.
+ *
+ * This software is the confidential and proprietary information of SAP
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with SAP.
+ */
+package de.hybris.training.b2b.cockpits.constants;
+
+/**
+ * Global class for all B2bCockpits constants. You can add global constants for your extension into this class.
+ */
+public final class B2bCockpitsConstants extends GeneratedB2bCockpitsConstants
+{
+	public static final String EXTENSIONNAME = "b2bcockpits";
+
+	public static final String JASPER_REPORTS_MEDIA_FOLDER = "jasperreports";
+
+	private B2bCockpitsConstants()
+	{
+		// empty
+	}
+}
