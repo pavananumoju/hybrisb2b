23d84cc08bda4b7ba0427d5356cdd8e09789d4cf
pavananumoju <pavananumoju@gmail.com>
2022-02-27 23:33:54 +0530

    Event and Listener changes

--- bin/custom/b2b/b2bcore/src/de/hybris/training/b2b/core/event/AddToCartEvent.java
+++ bin/custom/b2b/b2bcore/src/de/hybris/training/b2b/core/event/AddToCartEvent.java
@@ -1,2 +1,15 @@
-package de.hybris.training.b2b.core.event;public class AddToCartEvent {
+package de.hybris.training.b2b.core.event;
+
+import de.hybris.platform.servicelayer.event.events.AbstractEvent;
+
+public class AddToCartEvent extends AbstractEvent {
+    private String fullName;
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
 }
--- bin/custom/b2b/b2bcore/src/de/hybris/training/b2b/core/event/AddToCartEventListener.java
+++ bin/custom/b2b/b2bcore/src/de/hybris/training/b2b/core/event/AddToCartEventListener.java
@@ -1,2 +1,11 @@
-package de.hybris.training.b2b.core.event;public class AddToCartEventListener {
+package de.hybris.training.b2b.core.event;
+
+import de.hybris.platform.servicelayer.event.impl.AbstractEventListener;
+
+public class AddToCartEventListener extends AbstractEventListener<AddToCartEvent> {
+    @Override
+    protected void onEvent(AddToCartEvent addToCartEvent) {
+        String fullName = addToCartEvent.getFullName();
+        System.out.println("EventTriggered: " + fullName);
+    }
 }
